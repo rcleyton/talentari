690ca76039b8a940cda33dda56cbd1d321a1d428
Cleyton Silva <silva.rcleyton@gmail.com>
2025-01-21 12:45:39 -0300

    Carregando .env em produção

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Talentari
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    # Load environment variables from .env.production if running in production
+    if Rails.env.production?
+      require "dotenv"
+      Dotenv.load(".env.production")
+    end
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
